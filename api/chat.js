327ed7c6bbebb2740a2d4fa472bca691944fcf6c
Pedro Henrique <pedroenriquefreitassilavan@gmail.com>
2026-06-17 08:32:51 -0300

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,15 +1,5 @@
-import OpenAI from "openai";
-
 export default async function handler(req, res) {
-  try {
-    return res.status(200).json({
-      ok: true,
-      hasKey: !!process.env.OPENAI_API_KEY,
-      node: process.version
-    });
-  } catch (err) {
-    return res.status(500).json({
-      error: err.message
-    });
-  }
+  return res.status(200).json({
+    reply: "API funcionando!"
+  });
 }
